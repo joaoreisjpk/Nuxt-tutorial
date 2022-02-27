c45e82d761bba4fb65ebc55aeac5053d246ea4a6
João Reis <joaoreisjpk@gmail.com>
2022-02-27 11:51:28 -0300

    creating the products page

---
+++ pages/products/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <Nav />
+    <div class="container">
+      <LargeCardDisplay
+        v-for="cardInfo in largeCardInfo.slice(0, 1)"
+        :key="cardInfo.id"
+        :cardsSection="cardInfo"
+      />
+      <SmallCardDisplay
+        v-for="cardInfo in smallCardInfo.slice(0, 1)"
+        :key="cardInfo.id"
+        :cardsSection="cardInfo"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import { largeCardSections, smallCardSections } from "@/store/data.ts";
+export default {
+  data() {
+    return {
+      largeCardInfo: largeCardSections,
+      smallCardInfo: smallCardSections,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
