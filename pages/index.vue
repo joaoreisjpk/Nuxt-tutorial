b621b0c4e8ec82cf679cf01d29e79faa3f572646
João Reis <joaoreisjpk@gmail.com>
2022-02-27 11:33:35 -0300

    creating the Header

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
-    <Hero />
-    <LargeCardDisplay
-      v-for="cardInfo in largeCardInfo"
-      :key="cardInfo.id"
-      :cardsSection="cardInfo"
-    />
-    <SmallCardDisplay
-      v-for="cardInfo in smallCardInfo"
-      :key="cardInfo.id"
-      :cardsSection="cardInfo"
-    />
+    <Nav />
+    <div class="container">
+      <Hero />
+      <LargeCardDisplay
+        v-for="cardInfo in largeCardInfo"
+        :key="cardInfo.id"
+        :cardsSection="cardInfo"
+      />
+      <SmallCardDisplay
+        v-for="cardInfo in smallCardInfo"
+        :key="cardInfo.id"
+        :cardsSection="cardInfo"
+      />
+    </div>
   </div>
 </template>
 
