4692643cb30c126979b85c5b984223db49eb9e48
João Reis <joaoreisjpk@gmail.com>
2022-02-26 21:52:11 -0300

    creating the smallCardDisplay component

--- pages/index.vue
+++ pages/index.vue
@@ -6,18 +6,24 @@
       :key="cardInfo.id"
       :cardsSection="cardInfo"
     />
+    <SmallCardDisplay
+      v-for="cardInfo in smallCardInfo"
+      :key="cardInfo.id"
+      :cardsSection="cardInfo"
+    />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import { largeCardSections } from "../store/data";
+import { largeCardSections, smallCardSections } from "../store/data";
 
 export default Vue.extend({
   name: "IndexPage",
   data() {
     return {
       largeCardInfo: largeCardSections,
+      smallCardInfo: smallCardSections,
     };
   },
 });
